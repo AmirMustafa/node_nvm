f548f819a15d5b9ad6679923001570b8ec4f2c36
Amir Mustafa <amirmustafa@Amirs-MacBook-Pro.local>
2023-09-23 23:34:07 +0530

    feat: get inspirational quotes - random

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const inspirationQuote = require('inspirational-quotes');
 const app = express();
 const port = 5000;
 
+// Serve static files from the "views" directory
+app.use(express.static('views'));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     return res.send('Hello World !');
+});
+
+app.get('/quote', (req, res) => {
+    const quote = inspirationQuote.getRandomQuote();
+    return res.json({ quote });
 });
 
 app.listen(port, () => {
